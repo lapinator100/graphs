b3d97e0de5086b9c7f52abf749b29d4b8fcba728
Steffen Ryll <steffen.ryll@me.com>
2016-02-20 18:24:20 +0100

    Implement breadth-first search

--- app.pas
+++ app.pas
@@ -76,6 +76,76 @@ begin
   end;
 end;
 
+function isVertexInNodeList(node: Integer; nodeList: TNodeList): Boolean;
+var
+  i: Integer;
+begin
+  for i := 0 to length(nodeList) - 1 do
+    if nodeList[i] = node then
+    begin
+      result := True;
+      exit;
+    end;
+
+  result := False;
+end;
+
+function getNextNeighbourNode(node: Integer; exludeNodes: TNodeList): Integer;
+var
+  y: Integer;
+begin
+  for y := 0 to length(matrix[node]) - 1 do
+    if matrix[node, y] = Connected then
+    begin
+      if not isVertexInNodeList(y, exludeNodes) then
+      begin
+        result := y;
+        exit;
+      end;
+    end;
+
+  result := -1;
+end;
+
+function breadthFirstSearch: TEdgeList;
+var
+  currentNode, nextNeighbourNode: Integer;
+  activeNodes, processedNodes: TNodeList;
+  currentEdge: TEdge;
+  edges: TEdgeList;
+begin
+  setLength(activeNodes, 1);
+  activeNodes[0] := 0;
+  setLength(processedNodes, 1);
+  processedNodes[0] := 0;
+
+  while length(activeNodes) <> 0 do
+  begin
+    currentNode := activeNodes[0];
+    nextNeighbourNode := getNextNeighbourNode(currentNode, processedNodes);
+
+    if nextNeighbourNode = -1 then
+    begin
+      Move(activeNodes[1], activeNodes[0], SizeOf(activeNodes[0]) * (Length(activeNodes) - 1));
+      SetLength(activeNodes, Length(activeNodes) - 1);
+    end
+    else
+    begin
+      setLength(activeNodes, length(activeNodes) + 1);
+      activeNodes[length(activeNodes) - 1] := nextNeighbourNode;
+      setLength(processedNodes, length(processedNodes) + 1);
+      processedNodes[length(processedNodes) - 1] := nextNeighbourNode;
+
+      currentEdge.first := currentNode;
+      currentEdge.second := nextNeighbourNode;
+      setLength(edges, length(edges) + 1);
+      edges[length(edges) -1] := currentEdge;
+    end;
+  end;
+
+  result := edges;
+end;
+
 {$R *.lfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -117,8 +187,13 @@ begin
 end;
 
 procedure TForm1.btn_treeClick(Sender: TObject);
+var edges: TEdgeList;
 begin
-  breadthFirstSearch();
+  edges := breadthFirstSearch();
+
+  clearMatrix;
+  applyEdgesToMatrix(edges);
+  refreshGrid;
 end;
 
 procedure TForm1.refreshGrid;
